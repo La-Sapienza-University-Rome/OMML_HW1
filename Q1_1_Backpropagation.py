Add Backpropagation code. Gradients are correct, but it does not give a good optimization

Q1_1_Backpropagation.py
@@ -30,6 +30,25 @@ def feedforward(X, W, b, v, sigma):
     
     return pred
     
+def backpropagation(X, W, b, v, sigma, rho):
+    
+    grads = {}
+    
+    P = len(X)
+    linear_layer = (np.dot(X, W) + b)
+    a_2 = tanh(linear_layer, sigma)
+    dJdf = (1/P)*(np.dot(a_2, v) - y)
+    dtanh = 1 - tanh(linear_layer, sigma)**2
+    
+    dW1_1 = np.tensordot(dJdf, np.transpose(v), axes=0)
+    dW1_2 = dW1_1*dtanh
+    
+    grads['v'] = np.dot(dJdf, a_2) + rho*v
+    grads['b'] = np.sum(dW1_2, axis=0) + rho*b
+    grads['W'] = np.tensordot(np.transpose(X), dW1_2, axes=1) + rho*W
+    
+    return grads
+    
 def loss(x0, funcArgs):
     
     X = funcArgs[0]
@@ -67,18 +86,32 @@ def feedforwardplot(x1, x2, W, b, v, sigma):
     
     return pred
 
-def train(X, y, sigma, N, rho, W_init, b_init, v_init, max_iter=1000, 
-          tol=1e-5, method='CG', func=loss):
+def train(X, y, sigma, N, rho, W, b, v, max_iter=1000, 
+          tol=1e-5, learning_rate=1e-3):
     
-    x0 = np.concatenate((W_init, b_init, v_init), axis=None)
     funcArgs = [X, y, sigma, N, rho]
-
-    res = minimize(func,
-                   x0,
-                   args=funcArgs, 
-                   method=method, 
-                   tol=tol,
-                   options={'maxiter':max_iter, 'eps':1e-3})    
+    loss_hist = 10000
+    #norm_hist = 100000
+    tol_bool = False
+    for it in range(max_iter):
+        x0 = np.concatenate((W, b, v), axis=None)
+        
+        # Compute loss and gradients using the current minibatch
+        loss_train = loss(x0, funcArgs)
+        grads = backpropagation(X=X, W=W, b=b, v=v, sigma=sigma, rho=rho)
+        
+        W += -learning_rate * grads['W']
+        b += -learning_rate * grads['b']
+        v += -learning_rate * grads['v'] 
+        
+        # Tolerance stop condition
+        if abs(loss_train - loss_hist) < tol:    
+            res = {'W': W, 'b': b, 'v': v, 'loss_train': loss_train, 'tol': True, 'iter': it}
+            
+            return res
+        loss_hist = loss_train
+        
+    res = {'W': W, 'b': b, 'v': v, 'loss_train': loss_train, 'tol': tol_bool, 'iter': max_iter}
     
     return res
     
@@ -106,14 +139,16 @@ def plotting(W, b, v, sigma):
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    ax.set_title('F(x) learnt from MLP Q1_1')
+    ax.set_title('F(x) learnt from MLP BackPropagation')
     plt.show()
 
-sigma_grid = [1, 2, 5]
-N_grid = [10]
-rho_grid = np.linspace(1e-5, 1e-3, 2)
+sigma_grid = [1]
+N_grid = [4]
+rho_grid = [0]
+#rho_grid = np.linspace(1e-5, 1e-3, 2)
 iterables = [sigma_grid, N_grid, rho_grid]
 min_loss = 10000
+learning_rate=1e-2
 
 W = np.random.randn(X.shape[1], N_grid[0])
 b = np.random.randn(N_grid[0])
@@ -135,24 +170,23 @@ for t in itertools.product(*iterables):
     start = time.time()
     res = train(X, y, sigma=t[0], 
                 N=t[1], rho=t[2], 
-                W_init=W, b_init=b, v_init=v,
-                max_iter=5000, tol=1e-6, 
-                method='CG', func=loss)
+                W=W, b=b, v=v,
+                max_iter=5000, tol=1e-8,
+                learning_rate=learning_rate)
     stop = time.time()
     
     res_loss = loss_test(X=X_test, y=y_test, 
                          sigma=t[0], N=t[1], 
                          rho=t[2], 
-                         W=res.x[:X.shape[1]*N].reshape((X.shape[1],N)),
-                         b=res.x[X.shape[1]*N:X.shape[1]*N+N],
-                         v=res.x[X.shape[1]*N+N:])
+                         W=res['W'],
+                         b=res['b'],
+                         v=res['v'])
                    
     print('')   
     print('Time required by optimization:', round(stop-start, 1), ' s')
     print('Validation Loss: ', res_loss)
-    print('Minimal Loss Value on Train: ', res.fun)
-    print('Num Iterations: ', res.nit)
-    print('Did it converge?: ', res.success)
+    print('Minimal Loss Value on Train: ', res['loss_train'])
+    print('Iterations: ', res['iter'])
     print('===================')
                    
     if res_loss < min_loss:
@@ -160,11 +194,11 @@ for t in itertools.product(*iterables):
         sigma_best = t[0]
         rho_best = t[2]
         min_loss = res_loss
-        best_params = res.x
+        best_params = res
 
-W=best_params[:X.shape[1]*N_best].reshape((X.shape[1],N_best))
-b=best_params[X.shape[1]*N_best:X.shape[1]*N_best+N_best]
-v=best_params[X.shape[1]*N_best+N_best:]
+W=best_params['W']
+b=best_params['b']
+v=best_params['v']
 
 print('N')
 print(N_best)
@@ -183,5 +217,12 @@ print(b)
 print('')
 print('v')
 print(v)
+print('')
+print('Loss')
+print(min_loss)
+print('')
+print('Tolerance Reached?')
+print(res['tol'])
+
 
 plotting(W, b, v, sigma_best)