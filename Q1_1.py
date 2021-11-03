1135554bad474a26b9d112f93367bdbe04bc15dd
juanmatanaranjo <juan.mnaranjo@hotmail.com>
2021-11-03 22:43:33 +0100

    Continue work on Q1

--- Q1_1.py
+++ Q1_1.py
@@ -6,8 +6,6 @@ import itertools
 import matplotlib.pyplot as plt
 import time
 
-np.random.seed(42)
-
 df = pd.read_csv('DATA.csv')
 
 train, test = train_test_split(df, test_size=0.255, random_state=1939671)
@@ -15,8 +13,8 @@ train, test = train_test_split(df, test_size=0.255, random_state=1939671)
 X = np.array(train[['x1', 'x2']])
 y = np.array(train['y'])
 
-X_test = np.array(train[['x1', 'x2']])
-y_test = np.array(train['y'])
+X_test = np.array(test[['x1', 'x2']])
+y_test = np.array(test['y'])
 
 
 def tanh(s, sigma):
@@ -58,26 +56,7 @@ def backpropagation(x0, funcArgs):
 
     return np.concatenate((dW, db, dv), axis=None)
 
-# def backpropagationold(X, W, b, v, sigma, rho):
-    # grads = {}
-
-    # P = len(X)
-    # linear_layer = (np.dot(X, W) + b)
-    # a_2 = tanh(linear_layer, sigma)
-    # dJdf = (1 / P) * (np.dot(a_2, v) - y)
-    # dtanh = 1 - tanh(linear_layer, sigma) ** 2
-
-    # dW1_1 = np.tensordot(dJdf, np.transpose(v), axes=0)
-    # dW1_2 = dW1_1 * dtanh
-
-    # grads['v'] = np.dot(dJdf, a_2) + rho * v
-    # grads['b'] = np.sum(dW1_2, axis=0) + rho * b
-    # grads['W'] = np.tensordot(np.transpose(X), dW1_2, axes=1) + rho * W
-
-    # return grads
-
-
-def loss(x0, funcArgs):
+def loss(x0, funcArgs, test=False):
     X = funcArgs[0]
     y = funcArgs[1]
     sigma = funcArgs[2]
@@ -91,12 +70,15 @@ def loss(x0, funcArgs):
     P = len(y)
     norm = np.linalg.norm(x0)
     pred = feedforward(X, W, b, v, sigma)
-    res = ((np.sum((pred - y) ** 2)) * P ** (-1) + rho * norm**2) * 0.5
+    if test:
+        res = ((np.sum((pred - y) ** 2)) * P ** (-1)) * 0.5
+    else:
+        res = ((np.sum((pred - y) ** 2)) * P ** (-1) + rho * norm ** 2) * 0.5
 
     return res
 
 
-def loss_test(X, y, sigma, N, rho, W, b, v):
+def loss_test(X, y, sigma, W, b, v):
     P = len(y)
     pred = feedforward(X, W, b, v, sigma)
     res = ((np.sum((pred - y) ** 2)) * P ** (-1)) * 0.5
@@ -151,24 +133,25 @@ def plotting(W, b, v, sigma):
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
-    ax.set_title('F(x) learnt from MLP BackPropagation')
+    ax.set_title('F(x) learnt from MLP')
     plt.show()
 
-
-sigma_grid = [0.5, 1, 2, 5]
-N_grid = [2, 5, 10]
+sigma_grid = [0.5, 1, 5, 10]
+N_grid = [10, 20, 40, 50, 60]
 rho_grid = np.linspace(1e-5, 1e-3, 3)
-iterables = [sigma_grid, N_grid, rho_grid]
+method_grid = ['CG', 'BFGS', 'L-BFGS-B']
+iterables = [sigma_grid, N_grid, rho_grid, method_grid]
 min_loss = 10000
+val_loss = []
+train_loss = []
 
 for t in itertools.product(*iterables):
 
     N = t[1]
-    W = np.random.randn(X.shape[1], N)
-    b = np.random.randn(N)
-    v = np.random.randn(N)
-    
-    
+    W = np.random.normal(size=(X.shape[1], N))
+    b = np.random.normal(size=N)
+    v = np.random.normal(size=N)
+
     x0 = np.concatenate((W, b, v), axis=None)
 
     print('===================')
@@ -181,31 +164,29 @@ for t in itertools.product(*iterables):
                 N=N, rho=t[2], 
                 W=W, b=b, v=v,
                 max_iter=5000, tol=1e-6, 
-                method='CG', func=loss)
+                method=t[3], func=loss)
     stop = time.time()
 
-    res_loss = loss_test(X=X_test, y=y_test,
-                         sigma=t[0], N=N,
-                         rho=t[1],
-                         W=res.x[:int(X.shape[1] * N)].reshape((X.shape[1], N)),
-                         b=res.x[int(X.shape[1] * N):int(X.shape[1] * N + N)],
-                         v=res.x[int(X.shape[1] * N + N):])
+    funcArgs_test = [X_test, y_test, t[0], N, t[2]]
+
+    val_loss = loss(res.x, funcArgs_test, test=True)
 
     print('')
     print('Time required by optimization:', round(stop - start, 1), ' s')
-    print('Validation Loss: ', res_loss)
+    print('Validation Loss: ', val_loss)
     print('Minimal Loss Value on Train: ', res.fun)
     print('Iterations: ', res.nit)
     print('Did it converge?:', res.success)
     print('===================')
 
-    if res_loss < min_loss:
+    if val_loss < min_loss:
         N_best = N
         sigma_best = t[0]
         rho_best = t[2]
-        min_loss = res_loss
+        min_loss = val_loss
         best_params = res.x
         convergence = res.success
+        method_best = t[3]
 
 W = best_params[:int(X.shape[1] * N_best)].reshape((X.shape[1], N_best))
 b = best_params[int(X.shape[1] * N_best):int(X.shape[1] * N_best + N_best)]
@@ -234,6 +215,9 @@ print(min_loss)
 print('')
 print('Convergence?')
 print(convergence)
+print('')
+print('Best Method?')
+print(method_best)
 
 plotting(W, b, v, sigma_best)
 
--- Q1_2.py
+++ Q1_2.py
@@ -13,8 +13,9 @@ train, test = train_test_split(df, test_size=0.255, random_state=1939671)
 X = np.array(train[['x1', 'x2']])
 y = np.array(train['y'])
 
-X_test = np.array(train[['x1', 'x2']])
-y_test = np.array(train['y'])
+X_test = np.array(test[['x1', 'x2']])
+y_test = np.array(test['y'])
+
 
 def rbf(X, c, sigma):
     """
@@ -30,6 +31,7 @@ def rbf(X, c, sigma):
 
     return np.exp(-(np.linalg.norm(minus_matrix, axis=2)/sigma)**2)
 
+
 def feedforward(X, c, v, sigma):
     """
     This function is only applied for a single observation
@@ -41,7 +43,8 @@ def feedforward(X, c, v, sigma):
     
     pred = np.dot(rbf(X, c, sigma).T, v)
     return pred
-    
+
+
 def backpropagation(x0, funcArgs):
 
     X = funcArgs[0]
@@ -69,8 +72,9 @@ def backpropagation(x0, funcArgs):
     dc = np.sum(dzdc*dW1_1, axis=1) + rho*c
 
     return np.concatenate((dc, dv), axis=None)
-    
-def loss(x0, funcArgs):
+
+
+def loss(x0, funcArgs, test=False):
     
     X = funcArgs[0]
     y = funcArgs[1]
@@ -82,24 +86,30 @@ def loss(x0, funcArgs):
     v = x0[int(X.shape[1]*N):]
 
     P = len(y)
-    sum_ = np.sum((feedforward(X, c, v, sigma) - y)**2)
+    pred = feedforward(X, c, v, sigma)
     norm = np.linalg.norm(x0)
-    res = (sum_*P**(-1) + rho*norm**2)*0.5 
+    if test:
+        res = ((np.sum((pred - y) ** 2)) * P ** (-1)) * 0.5
+    else:
+        res = ((np.sum((pred - y) ** 2)) * P ** (-1) + rho * norm ** 2) * 0.5
     
     return res
-    
-def loss_test(X, y, sigma, N, rho, c, v):
+
+
+def loss_test(X, y, sigma, c, v):
 
     P = len(y)
     res = np.sum((feedforward(X, c, v, sigma) - y)**2)*0.5*P**(-1)
     
     return res
-    
+
+
 def feedforwardplot(x_i_1, x_i_2, c, v, sigma):
     x_i = np.array([x_i_1, x_i_2])
     pred = np.dot(np.exp(-(np.linalg.norm((x_i - c.T), axis=1)/sigma)**2), v)
     return pred
-    
+
+
 def train(X, y, sigma, N, rho, c_init, 
           v_init, max_iter=1000, tol=1e-5, method='CG', func=loss):
     
@@ -115,7 +125,8 @@ def train(X, y, sigma, N, rho, c_init,
                    options={'maxiter':max_iter})    
     
     return res
-    
+
+
 def plotting(c, v, sigma):
     
 
@@ -142,18 +153,20 @@ def plotting(c, v, sigma):
     ax.set_zlabel('z')
     ax.set_title('F(x) learnt from RBS')
     plt.show()
-    
-sigma_grid = [0.01, 1, 2]
-N_grid = [2, 5, 10, 20, 40]
-rho_grid = [1e-5, 1e-4, 1e-3]
-iterables = [sigma_grid, N_grid, rho_grid]
+
+
+sigma_grid = [0.5, 1, 5, 10]
+N_grid = [10, 20, 40, 50, 60]
+rho_grid = np.linspace(1e-5, 1e-3, 3)
+method_grid = ['CG', 'BFGS', 'L-BFGS-B']
+iterables = [sigma_grid, N_grid, rho_grid, method_grid]
 min_loss = 10000
 
 for t in itertools.product(*iterables):
 
     N = t[1]
-    c = np.random.randn(X.shape[1], N)
-    v = np.random.randn(N)
+    c = np.random.normal(size=(X.shape[1], N))
+    v = np.random.normal(size=N)
 
     x0 = np.concatenate((c, v), axis=None)
     
@@ -167,30 +180,29 @@ for t in itertools.product(*iterables):
                 N=t[1], rho=t[2], 
                 c_init=c, v_init=v,
                 max_iter=5000, tol=1e-6, 
-                method='CG', func=loss)
+                method=t[3], func=loss)
     stop = time.time()
-    
-    res_loss = loss_test(X=X_test, y=y_test, 
-                         sigma=t[0], N=t[1], 
-                         rho=t[2], 
-                         c=res.x[:X.shape[1]*N].reshape((X.shape[1],N)),
-                         v=res.x[X.shape[1]*N:])
+
+    funcArgs_test = [X_test, y_test, t[0], N, t[2]]
+
+    val_loss = loss(res.x, funcArgs_test, test=True)
                    
     print('')    
     print('Time required by optimization:', round(stop-start, 1), ' s')
-    print('Validation Loss: ', res_loss)
+    print('Validation Loss: ', val_loss)
     print('Minimal Loss Value', res.fun)
     print('Num Iterations', res.nit)
     print('Did it converge?:', res.success)
     print('===================')
                    
-    if res_loss < min_loss:
+    if val_loss < min_loss:
         N_best = N
         sigma_best = t[0]
         rho_best = t[2]
         min_loss = res.fun
         best_params = res.x
         convergence = res.success
+        method_best = t[3]
 
 c=best_params[:X.shape[1]*N_best].reshape((X.shape[1],N_best))
 v=best_params[X.shape[1]*N_best:]
@@ -215,6 +227,9 @@ print(min_loss)
 print('')
 print('Convergence?')
 print(convergence)
+print('')
+print('Best Method?')
+print(method_best)
 
 plotting(c, v, sigma_best)
 
