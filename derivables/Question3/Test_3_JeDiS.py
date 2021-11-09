27805b8df97da39ec9196f775041b9083bd9faaa
damjimenezgu <47650665+damjimenezgu@users.noreply.github.com>
2021-11-09 23:13:37 +0100

    Definition of files
    Definition of files

--- derivables/Question3/Test_3_JeDiS.py
+++ derivables/Question3/Test_3_JeDiS.py
@@ -1,1 +1,41 @@
+import numpy as np
+import os
+import sys
+parentdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+change = parentdir + r'\functions'
+sys.path.append(change)
+from functions import *
+import pickle
+import pandas as pd
 
+# Load data set
+X_test = pd.read_csv('../../DATA.csv')
+X_test = np.array(X_test[['x1', 'x2']])
+
+def ICanGeneralize(x_new):
+    """
+    Make prediction with defined parameters and new data
+
+    :param x_new: Input data with two features
+
+    :return predictions
+    """
+
+    # Load the parameters needed
+    with open('q3_values_for_prediction.pickle', 'rb') as handle:
+        dict = pickle.load(handle)
+    W = dict['W']
+    b = dict['b']
+    v = dict['v']
+    sigma = dict['sigma']
+    x_new = np.array(x_new)
+
+    # Compute prediction
+    linear_layer = (np.dot(x_new, W) + b)
+    activation = tanh(linear_layer, sigma)
+    pred = np.dot(activation, v)
+    return pred
+
+# Execute function to make predictions
+y_pred = ICanGeneralize(X_test)
+print(y_pred)
