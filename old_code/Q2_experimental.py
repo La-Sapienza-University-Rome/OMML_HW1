210c08cdb7ff5e0f1fa1f139705f1906e6a49ab8
stdrr <development.stdrr@gmail.com>
2021-11-10 10:27:50 +0100

    Reorganize code into modules

--- old_code/Q2_experimental.py
+++ old_code/Q2_experimental.py
@@ -1,14 +1,15 @@
-import numpy as np
-import cvxpy as cvx
-import time
+import abc
+import copy
 import json
-from scipy.optimize import least_squares, minimize
+import time
+
+import cvxpy as cvx
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import least_squares
 from scipy.stats import truncnorm
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
 from tqdm import tqdm
-import abc
-
 
 np.random.seed(1939671)
 
@@ -22,7 +23,7 @@ class Model(abc.ABC):
     The public API is [see the description of each function for details]:
     - eval(tuple(X_test,y_test))
     - feedforward(X)
-    - fit(tuple(X,y), tuple(X_val,y_val), trials=1, **kwargs)
+    - fit(tuple(X,y), tuple(X_test,y_test), trials=1, **kwargs)
     - loss(X, y, test=False)
     - plot()
     - __str__() -> returns the string representation of the state of the object
@@ -35,9 +36,7 @@ class Model(abc.ABC):
     def _load_hyperparameters(self, hyper_param_cfg_file):
         """
         Load the hyperparameters' values of Q1.
-
         :param hyper_param_cfg_file: path to the configuration file
-
         :return configuration dictionary
         """
         with open(hyper_param_cfg_file, 'r') as h_cfg_file:
@@ -48,7 +47,6 @@ class Model(abc.ABC):
     def _set_hyperparameters(self, hyper_cfg_dict):
         """
         Set the hyperparameters' values.
-
         :param hyper_cfg_dict: configuration dictionary
         """
         self.N = hyper_cfg_dict['N']
@@ -59,9 +57,7 @@ class Model(abc.ABC):
     def _tanh(self, s):
         """
         Compute the tanh with variable SIGMA.
-
         :param s: argument to tanh(s)
-
         :retuern tanh(s)
         """
         prod = 2 * self.SIGMA * s
@@ -71,9 +67,7 @@ class Model(abc.ABC):
     def _rbf(self, X):
         """
         Compute the rbf with variable SIGMA.
-
         :param X: argument to rbf(X)
-
         :return rbf(X)
         """
         rows_idxs = np.array([(i,) for i in range(self.N)])
@@ -94,8 +88,7 @@ class Model(abc.ABC):
     def _set_state(self, **kwargs):
         """
         Set the state of the model. Sample W,b,c
-
-        :param kwargs: variable arguments, see ExtremeLearning.fit() for details
+        :param kwargs: variable arguments, see TwoBlocksContext.fit() for details
         """
         if self.algorithm == 'MLP':
             lbound_W = kwargs['lbound_W'] if 'lbound_W' in kwargs.keys() else -4
@@ -108,8 +101,8 @@ class Model(abc.ABC):
             std_b = kwargs['std_b'] if 'std_b' in kwargs.keys() else 3.5
             self.W = truncnorm.rvs(a=lbound_W, b=ubound_W, 
                                     loc=mean_W, scale=std_W, size=(self.X.shape[1], self.N))
-            self.b = np.random.uniform(lbound_b, ubound_b, size=(1, self.N))#truncnorm.rvs(a=lbound_b, b=ubound_b, 
-                     #               loc=mean_b, scale=std_b, size=(1, self.N))
+            self.b = truncnorm.rvs(a=lbound_b, b=ubound_b, loc=mean_b, scale=std_b, size=(1, self.N))
+                    #np.random.uniform(lbound_b, ubound_b, size=(1, self.N))
         else:
             if kwargs['centers_selection'] == 'random':
                 c_idxs = np.random.choice(np.arange(self.X.shape[0]), 
@@ -123,18 +116,16 @@ class Model(abc.ABC):
     def plot(self, title):
         """
         Plot the function in (-3,3)x(-2,2).
-
         :param title
         """
         fig = plt.figure(figsize=(12, 8))
         ax = plt.axes(projection='3d')
-        x = np.linspace(-3, 3, 50) 
-        y = np.linspace(-2, 2, 50)
-        X_plot, Y_plot = np.meshgrid(x, y) 
-        Z = np.array( [[self.feedforward(np.array([x1, x2])) for x2 in y] for x1 in x ])
-
-        ax.plot_surface(X_plot, Y_plot, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
-
+        xs = np.linspace(-2, 2, 50)
+        ys = np.linspace(-3, 3, 50) 
+        X, Y = np.meshgrid(xs, ys)
+        XY = np.column_stack([X.ravel(), Y.ravel()])
+        Z = self.feedforward(XY).reshape(X.shape)
+        ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
@@ -146,9 +137,7 @@ class Model(abc.ABC):
     def _feedforward_MLP(self, X):
         """
         Implement the feedforward pass of the Multilayer Perceptron
-
         :param X: observations[n_samples, n_features]
-
         :return f(X) 
         """
         pass
@@ -158,9 +147,7 @@ class Model(abc.ABC):
     def _feedforward_RBF(self, X):
         """
         Implement the feedforward pass of the Radial Basis Function network
-
         :param X: observations[n_samples, n_features]
-
         return f(X)
         """
         pass
@@ -170,8 +157,7 @@ class Model(abc.ABC):
     def _save_state(self, state, **kwargs):
         """
         Save the state of the model, e.g. the best parameters, the training time,
-        the training loss, the validation loss, ...
-        
+        the training loss, the test loss, ...
         :param state: current state
         :param kwargs: variable arguments; see overrided functions for details.
         """
@@ -182,11 +168,9 @@ class Model(abc.ABC):
     def loss(self, X, y, test=False):
         """
         Compute the loss on X. If test=False, the regularization term is not added.
-
         :param X: observations[n_samples, n_features]
         :param y: target values
         :param test: boolean
-
         :return loss value (float)
         """
         pass
@@ -196,25 +180,21 @@ class Model(abc.ABC):
     def feedforward(self, X):
         """
         Return the feedforward pass according to the selected algorithm.
-
         :param X: observations[n_samples, n_features]
-
         :return f(X)
         """
         pass
 
     
     @abc.abstractmethod
-    def fit(self, Xy, Xy_val, trials=1, **kwargs):
+    def fit(self, Xy, Xy_test, trials=1, **kwargs):
         """
         Run the model /trials/ times on the training data (X,y). Select the best configuration 
-        through evaluating the model om the validation data (X_val,y_val).
-
+        through evaluating the model om the test data (X_test,y_test).
         :param Xy: tuple(X,y)
-        :param Xy_val: tuple(X_val,y_val)
+        :param Xy_test: tuple(X_test,y_test)
         :param trials: number of random samplings
-        :param kwargs: variable parameters; see ExtremeLearning.fit() for details
-
+        :param kwargs: variable parameters; see TwoBlocksContext.fit() for details
         :return self 
         """
         pass
@@ -224,9 +204,7 @@ class Model(abc.ABC):
     def eval(self, Xy_test):
         """
         Evaluate the model on the new data (X_test, y_test). No regulazation is added to the loss
-
         :param Xy_test: tuple(X_test, y_test)
-
         :return loss value without the regularization term
         """
         pass
@@ -245,22 +223,23 @@ class ModelCVX(Model):
     def _feedforward_MLP(self, X):
         linear_layer = np.dot(X, self.W) + self.b
         activation = self._tanh(linear_layer)
-        return cvx.matmul(activation, self.v)
+        return cvx.matmul(activation, self.v.T)
 
 
     def _feedforward_RBF(self, X):
-        return cvx.matmul(self._rbf(X).T, self.v)
+        return cvx.matmul(self._rbf(X).T, self.v.T)
 
     
     def _set_state(self, **kwargs):
-        self.v = cvx.Variable(shape=(self.N, 1), name='v')
+        self.v = cvx.Variable(shape=(1,self.N), value=np.random.normal(size=(1,self.N)), name='v')
         super()._set_state(**kwargs)
 
 
     def _save_state(self, state, **kwargs):
         if kwargs['restore']:
             if self.algorithm == 'MLP':
-                self.W, self.b = self.state['best_W'], self.state['best_b']
+                self.W = self.state['best_W']
+                self.b = self.state['best_b']
             else:
                 self.c = self.state['best_c']
             self.v = self.state['best_v'].reshape(self.v.shape)
@@ -268,11 +247,12 @@ class ModelCVX(Model):
         else:
             if self.algorithm == 'MLP':
                 self.state = {'best_W': self.W.copy(),
-                                'best_b': self.b.copy()}
+                              'best_b': self.b.copy()}
             else:
                 self.state = {'best_c': self.c.copy()}
-            self.state['best_train_loss'] = state.value
-            self.state['best_val_loss'] = kwargs['val_loss']
+            self.state['best_v'] = np.array(self.v.value).ravel()
+            self.state['best_train_loss'] = kwargs['train_loss']
+            self.state['best_test_loss'] = kwargs['test_loss']
             self.state['N'] = self.N
             self.state['SIGMA'] = self.SIGMA
             self.state['RHO'] = self.RHO
@@ -280,7 +260,6 @@ class ModelCVX(Model):
             self.state['solve_time'] = state.solver_stats.solve_time
             self.state['num_iters'] = state.solver_stats.num_iters
             self.state['problem_status'] = state.status
-            self.state['best_v'] = np.array(self.v.value).ravel()
 
 
     def _feedforward(self, X):
@@ -291,7 +270,7 @@ class ModelCVX(Model):
 
 
     def feedforward(self, X):
-        return self._feedforward(X).value[0,0]
+        return np.squeeze(np.array(self._feedforward(X).value))
 
 
     def loss(self, X, y, test=False):
@@ -299,25 +278,26 @@ class ModelCVX(Model):
         pred = self._feedforward(X)
         res = cvx.sum_squares(pred - y) / (2*P)
         if not test:
-            res = res + 0.5 * self.RHO * cvx.square(cvx.norm2(self.v))
+            res = res + 0.5 * self.RHO * cvx.square(cvx.norm(self.v, 2))
         return res
 
 
-    def fit(self, Xy, Xy_val, trials=1, **kwargs):
+    def fit(self, Xy, Xy_test, trials=1, **kwargs):
         self.X, self.y = Xy
-        X_val, y_val = Xy_val
+        X_test, y_test = Xy_test
         max_iters = kwargs['max_iters'] if 'max_iters' in kwargs.keys() else 10000
         solver = kwargs['solver'] if 'solver' in kwargs.keys() else None
-        best_val_loss = 1e4
+        best_test_loss = 1e4
         start = time.time()
         for _ in tqdm(range(trials)):
             self._set_state(**kwargs)
             cvx_problem = cvx.Problem(cvx.Minimize(self.loss(X=self.X, y=self.y, test=False)))
             cvx_problem.solve(solver=solver, verbose=False, max_iters=max_iters)
-            current_val_loss = self.loss(X_val, y_val, test=False).value
-            if current_val_loss < best_val_loss:
-                best_val_loss = current_val_loss
-                self._save_state(cvx_problem, val_loss=best_val_loss, restore=False)
+            current_test_loss = self.loss(X_test, y_test, test=True).value
+            if current_test_loss < best_test_loss:
+                best_test_loss = current_test_loss
+                best_train_loss = self.loss(self.X, self.y, test=True) # train loss without the regularization term
+                self._save_state(cvx_problem, train_loss=best_train_loss, test_loss=best_test_loss, restore=False)
         stop = time.time()
         self._save_state(None, restore=True, total_elapsed_time=f'{round(stop-start, 3)}s')
         return self
@@ -376,7 +356,7 @@ class ModelNumpy(Model):
                 self.state = {'best_c': self.c.copy()}
             self.state['best_v'] = state.x
             self.state['best_train_loss'] = state.cost
-            self.state['best_val_loss'] = kwargs['val_loss']
+            self.state['best_test_loss'] = kwargs['test_loss']
             self.state['N'] = self.N
             self.state['SIGMA'] = self.SIGMA
             self.state['RHO'] = self.RHO
@@ -396,10 +376,8 @@ class ModelNumpy(Model):
         P = y.shape[0]
         pred = self.feedforward(X)
         res = np.sum((pred - y)**2) / (2*P)
-
         if not test:
             res = res + 0.5 * self.RHO * np.linalg.norm(self.v)**2
-
         return res
 
 
@@ -412,31 +390,28 @@ class ModelNumpy(Model):
         self.v = np.expand_dims(v, axis=1)
         if self.algorithm == 'MLP':
             linear_layer = np.dot(self.X, self.W) + self.b
-            a1 = self._tanh(linear_layer)
+            A = self._tanh(linear_layer)
         else:
-            a1 = self._rbf(self.X).T
+            A = self._rbf(self.X).T
         P = len(self.y)
-        z2 = np.dot(a1, self.v)
-        dJdf = (z2 - self.y) / P
-        dz2dv = a1
-        return np.concatenate([dz2dv, np.full(shape=(self.N, self.N), fill_value=np.sqrt(self.RHO * P))])/P # np.dot(dz2dv.T, dJdf) + self.RHO * self.v
+        return np.concatenate([A, np.full(shape=(self.N, self.N), fill_value=np.sqrt(self.RHO * P))])/P # return np.dot(dz2dv.T, dJdf) + self.RHO * self.v
 
 
-    def fit(self, Xy, Xy_val, trials=1, **kwargs):
+    def fit(self, Xy, Xy_test, trials=1, **kwargs):
         self.X, self.y = Xy
-        X_val, y_val = Xy_val
-        best_val_loss = 1e4
+        X_test, y_test = Xy_test
+        best_test_loss = 1e4
         start = time.time()
         for _ in tqdm(range(trials)):
             t0 = time.time()
             self._set_state(**kwargs)
             problem_res = least_squares(self._residuals, self.v, jac=self._gradient)
             self.v = problem_res.x
-            current_val_loss = self.loss(X_val, y_val, test=False)
+            current_test_loss = self.loss(X_test, y_test, test=False)
             t1 = time.time()
-            if current_val_loss < best_val_loss:
-                best_val_loss = current_val_loss
-                self._save_state(problem_res, val_loss=best_val_loss, solver_time=round(t1-t0, 5), restore=False)
+            if current_test_loss < best_test_loss:
+                best_test_loss = current_test_loss
+                self._save_state(problem_res, test_loss=best_test_loss, solver_time=round(t1-t0, 5), restore=False)
         stop = time.time()
         self._save_state(problem_res, restore=True, total_elapsed_time=f'{round(stop-start, 3)}s')
         return self
@@ -447,7 +422,9 @@ class ModelNumpy(Model):
         return self.loss(X_test, y_test, test=True)
 
 
-class ExtremeLearning:
+
+
+class TwoBlocksContext:
     """
     Context class for the Question 2, internally selects the library (CVXPY or Numpy) and 
     the algorithm (MLP or RBF) and provides a unique API to the client.
@@ -455,7 +432,7 @@ class ExtremeLearning:
 
     The public API is:
     - eval(tuple(X_test,y_test))
-    - fit(tuple(X,y), tuple(X_val,y_val), trials=1, **kwargs)
+    - fit(tuple(X,y), tuple(X_test,y_test), trials=1, **kwargs)
     - get_state()
     - loss(X, y, test=False)
     - plot()
@@ -497,13 +474,13 @@ class ExtremeLearning:
         return self.model.feedforward(X)
 
 
-    def fit(self, Xy, Xy_val, trials=1, **kwargs):
+    def fit(self, Xy, Xy_test, trials=1, **kwargs):
         """
         Run the model /trials/ times on the training data Xy and select the best
-        trial through evaluating the model on the validation data Xy_val.
+        trial through evaluating the model on the test data Xy_test.
 
         :param Xy: tuple (X, y); training data
-        :param Xy_val: tuple (X_val, y_val); validation data
+        :param Xy_test: tuple (X_test, y_test); test data
         :param trials: number of random samplings of W,b,c
         :param kwargs: further options for the fit:
                 :key lbound_W: lower bound of the interval of W
@@ -520,7 +497,7 @@ class ExtremeLearning:
         
         :return model
         """
-        return self.model.fit(Xy, Xy_val, trials, **kwargs)
+        return self.model.fit(Xy, Xy_test, trials, **kwargs)
 
     
     def loss(self, X, y, test=False):
@@ -555,4 +532,4 @@ class ExtremeLearning:
 
     
     def __str__(self):
-        return self.model.__str__()
+        return str(self.model)
